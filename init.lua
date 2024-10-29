63ae70fc67f6dc17b01b025493d3e3731aa7dd6f
Dan Chadwick <dan@danchadwickdesign.com>
2024-10-29 08:46:19 -0400

    Add debugger.

--- init.lua
+++ init.lua
@@ -18,19 +18,29 @@ local dap = require('dap')
 require('telescope').load_extension('dap')
 
 dap.adapters.php = {
-    type = "executable",
-    command = "node",
-    args = { os.getenv("HOME") .. "/vscode-php-debug/out/phpDebug.js" }
+  type = "executable",
+  command = "node",
+  args = { os.getenv("HOME") .. "/vscode-php-debug/out/phpDebug.js" }
 }
 
 dap.configurations.php = {
-    {
-        type = "php",
-        request = "launch",
-        name = "Listen for Xdebug",
-        port = 9003, 
-        log = true,
+  {
+    type = "php",
+    request = "launch",
+    name = "Listen for Xdebug",
+    port = 9003,
+    log = true,
+  },
+  {
+    name = "listen for Xdebug docker",
+    type = "php",
+    request = "launch",
+    port = 9003,
+    -- this is where your file is in the container
+    pathMappings = {
+      ["/var/www/html/web"] = "${workspaceFolder}"
     }
+  }
 }
 
 vim.keymap.set('n', '<leader>?', function() dap.continue() end)
@@ -41,29 +51,29 @@ vim.keymap.set('n', '<leader>b', function() dap.toggle_breakpoint() end)
 vim.keymap.set('n', '<leader>B', function() dap.set_breakpoint() end)
 vim.keymap.set('n', '<leader>dr', function() dap.repl.open() end)
 vim.keymap.set('n', '<leader>dl', function() dap.run_last() end)
-vim.keymap.set({'n', 'v'}, '<leader>dh', function()
-  require('dap.ui.widgets').hover()
-end)
-vim.keymap.set({'n', 'v'}, '<leader>dp', function()
-  require('dap.ui.widgets').preview()
-end)
-vim.keymap.set('n', '<leader>df', function()
-  local widgets = require('dap.ui.widgets')
-  widgets.centered_float(widgets.frames)
-end)
+-- vim.keymap.set({'n', 'v'}, '<leader>dh', function()
+--   require('dap.ui.widgets').hover()
+-- end)
+-- vim.keymap.set({'n', 'v'}, '<leader>dp', function()
+--   require('dap.ui.widgets').preview()
+-- end)
+-- vim.keymap.set('n', '<leader>df', function()
+--   local widgets = require('dap.ui.widgets')
+--   widgets.centered_float(widgets.frames)
+-- end)
 vim.keymap.set('n', '<leader>db', function()
   local widgets = require('dap.ui.widgets')
   widgets.centered_float(widgets.scopes)
 end)
-dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
-end
+-- dap.listeners.after.event_initialized["dapui_config"] = function()
+--   dapui.open()
+-- end
 
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+--   dapui.close()
+-- end
 
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
-end
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+--   dapui.close()
+-- end
 
